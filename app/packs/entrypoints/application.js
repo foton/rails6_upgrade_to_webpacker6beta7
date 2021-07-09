9df2b4686ab3ca1aa4352e425fa640e00ceb6bfa
foton <foton@centrum.cz>
2021-07-09 11:57:34 +0200

    fixed loading images  , RAILS JS commented out

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -3,14 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "src/channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 
 
@@ -22,7 +22,7 @@ function importAll(r) {
 //
 // This is needed, otherwise Webpacker throws:
 // Webpacker can't find media/images/logo.svg in /home/foton/workspace/webpacker_6/public/packs/manifest.json. Possible causes:
-importAll(require.context('../media/images/', true, /\.(svg|jpg|gif)$/));
+importAll(require.context('../images/', true, /\.(svg|jpg|gif)$/));
 
 
 console.log('Hello from Webpacker!')
